2aef7d670ed98635d2b59b22492263884fdadecb
sacredgeo <gilgocase@gmail.com>
2022-04-02 11:55:35 -0400

    Add files via upload

--- Image Effects/blocks/image2canvas/code.js
+++ Image Effects/blocks/image2canvas/code.js
@@ -10,9 +10,6 @@ runOnce(() => {
 const canvas = element.querySelector('#c')
 const ctx = canvas.getContext('2d')
 
-const width = state.imageData.width
-const height = state.imageData.height
-
 ctx.canvas.width = state.imageData.width
 ctx.canvas.height = state.imageData.height
 ctx.putImageData(state.imageData, 0, 0)
